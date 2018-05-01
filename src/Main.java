7d84c5a46f55330390aaeddba2eda62717329b35
Gopalakrishna Sharma <gsharma@adobe.com>
2018-05-01 14:52:48 +0530

    Read file names from arguments.

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,18 @@ class Main {
 
     public static void main(String [] args)
     {
+    	//src/testdata/Doc1.txt src/testdata/Doc2.txt src/testdata/Doc3.txt src/testdata/Doc4.txt src/testdata/Doc5.txt src/testdata/Doc6.txt
         try {
-            String[] fileNames = {
+            String[] fileNames = args; /*{
                     "src/testdata/Doc1.txt",
                     "src/testdata/Doc2.txt",
                     "src/testdata/Doc3.txt",
                     "src/testdata/Doc4.txt",
                     "src/testdata/Doc5.txt",
                     "src/testdata/Doc6.txt"
-            };
+            }; */
             TextParser parser = new TextParser(fileNames);
             parser.parse();
-
-
         }
         catch(Exception e)
         {
